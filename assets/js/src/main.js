57ee32edb5097d1ab0edae0d65e8659c955d0c78
unsalted <nick@unsalted.nu>
2015-02-26 20:12:53 -0800

    call json object

--- assets/js/src/main.js
+++ assets/js/src/main.js
@@ -3,7 +3,7 @@ $(function() {
   var deviceID    = "53ff6e066667574825562467";
   var accessToken = "1d5d8c64bdf0dee6d57030e372f9d6a7a9c682d5";
   //SENSOR NAME
-  var getVar = "temperature";
+  var getVar = "dataOut";
 
   var getURL = "https://api.spark.io/v1/devices/" +deviceID + "/" + getVar + "/";
 
@@ -11,8 +11,9 @@ $(function() {
     console.log('request');
     $.get(getURL,{ access_token: accessToken }, function(data){
       var obj = JSON.parse(data);
-      console.log(obj);
-      $('#output').html(obj.name+':&nbsp;'+obj.result);
+      var dtObj = JSON.parse(obj.result);
+      console.log(dtObj);
+      //$('#output').html(dtObj.name+':&nbsp;'+stObj.result);
     });
   },5000);
 
